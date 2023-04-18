25910281644fe18d6db698dbe564be29ee6fab63
Enric0Mz <enricovmarquezz@gmail.com>
2023-04-17 22:32:10 -0300

    fix(git): fixin .gitignore

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from fastapi import FastAPI
 
 from . import router
@@ -6,3 +8,6 @@ from . import router
 app = FastAPI(title="Neighbour Api", version="0.1.0", debug=True)
 
 app.include_router(router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
