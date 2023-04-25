0ae8cfc500c1baf3cdab74148304d6b915d8acfd
Enric0Mz <enricovmarquezz@gmail.com>
2023-04-24 22:18:31 -0300

    feat(common): initializating common module

---
+++ api/common/__init__.py
@@ -0,0 +1,7 @@
+from .page import BasePagination
+from .page import PageParams
+from .page import Details
+from .user import BaseUser
+
+
+__all__ = ['BasePagination', 'PageParams', 'Details', 'BaseUser',]
