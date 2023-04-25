3039d648180437b31e1cf460c2f546a489768c44
Enric0Mz <enricovmarquezz@gmail.com>
2023-04-24 22:34:03 -0300

    feat(products): adding list products route on domain

--- api/router.py
+++ api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from . import auth
 from . import register
 from . import user
+from . import products
 
 
 router = APIRouter(prefix="/api")
@@ -11,3 +12,4 @@ router = APIRouter(prefix="/api")
 router.include_router(register.router, prefix="/register", tags=["Register"])
 router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 router.include_router(user.router, prefix="/user", tags=["User"])
+router.include_router(products.router, prefix="/products", tags=["Products"])
